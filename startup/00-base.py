3e387abfd42782cdb63a5b2e54392cf566552afd
Thomas Hopkins <thopkins1@bnl.gov>
2025-07-17 14:44:45 -0400

    Eiger-async working with RunEngine

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,4 +1,5 @@
 import nslsii
+from bluesky.run_engine import RunEngine, autoawait_in_bluesky_event_loop
 
 nslsii.configure_base(
     get_ipython().user_ns,
@@ -6,3 +7,7 @@ nslsii.configure_base(
     publish_documents_with_kafka=True,
     redis_url="info.cdi.nsls2.bnl.gov",
 )
+
+RE = RunEngine()
+autoawait_in_bluesky_event_loop()
+
--- startup/20-areadetector.py
+++ startup/20-areadetector.py
@@ -1,4 +1,15 @@
+from pathlib import Path
+
 from cditools.eiger import EigerSingleTrigger
+from cditools.eiger_async import EigerDetector
+from ophyd_async.core import YMDPathProvider, UUIDFilenameProvider, init_devices
 
 
 eiger = EigerSingleTrigger(prefix="XF:31ID-ES{Eig-Det:1}", name="eiger")
+
+path_provider = YMDPathProvider(
+    filename_provider=UUIDFilenameProvider(),
+    base_directory_path=Path("/nsls2/data/tst/legacy/mock-proposals/2025-2/pass-56789/assets/eiger/")
+)
+with init_devices():
+    eiger_async = EigerDetector(prefix="XF:31ID-ES{Eig-Det:1}", path_provider=path_provider)
