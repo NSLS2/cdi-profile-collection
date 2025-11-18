9931fd64bba83b3931493b51032b7fbba0623f83
jennmald <jmaldonad@bnl.gov>
2025-11-18 15:11:10 -0500

    pre-commit

--- startup/18-screens.py
+++ startup/18-screens.py
@@ -7,6 +7,14 @@ cam_A2 = set_roiN_kinds(
 cam_A3 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{VPM-Cam:3}", name="cam_A3"))
 cam_A4 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{HPM-Cam:4}", name="cam_A4"))
 cam_A5 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{DM:2-Cam:5}", name="cam_A5"))
+cam_A6 = set_roiN_kinds(StandardProsilicaCam("XF:09IDB-BI{DM:3-Cam:6}", name="cam_A6"))
+cam_A7 = set_roiN_kinds(
+    StandardProsilicaCam("XF:09IDC-BI{FS:KBv-Cam:7}", name="cam_A7")
+)
+cam_A8 = set_roiN_kinds(
+    StandardProsilicaCam("XF:09IDC-BI{FS:KBh-Cam:8}", name="cam_A8")
+)
+cam_A9 = set_roiN_kinds(StandardProsilicaCam("XF:09IDC-BI{SMPL-Cam:9}", name="cam_A9"))
 
 vpm_fs = StandardScreen("XF:09IDA-OP:1{FS:VPM-Ax:Y}Mtr", name="screen_vpm")
 hpm_fs = StandardScreen("XF:09IDA-OP:1{FS:HPM-Ax:Y}Mtr", name="screen_hpm")
