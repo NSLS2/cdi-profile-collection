b9bc80deed2b90c07c7b4307d46260ea9ed1fa8c
jennmald <jmaldonad@bnl.gov>
2026-02-20 15:03:20 -0500

    update file paths for ophyd async

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -4,7 +4,12 @@ from cditools.eiger_async import EigerDetector
 from ophyd_async.core import UUIDFilenameProvider, YMDPathProvider, init_devices
 
 filename_provider = UUIDFilenameProvider()
-path_provider = YMDPathProvider(filename_provider, PosixPath("/tmp"))
+path_provider = YMDPathProvider(
+    filename_provider,
+    PosixPath(
+        f"/nsls2/data/cdi/proposals/{RE.md['cycle']}/{RE.md['data_session']}/assets/eiger2-1/%Y/%m/%d/"  # noqa: F821
+    ),
+)
 
 with init_devices(mock=True):
     eiger = EigerDetector(
