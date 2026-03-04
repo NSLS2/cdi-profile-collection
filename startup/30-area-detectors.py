73ee796b8fa940c73301315cbbe391ae39d1cc25
jennmald <jmaldonad@bnl.gov>
2026-03-04 11:15:57 -0500

    fix path for ophyd_async import and remove mocking

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -1,10 +1,10 @@
 from cditools.eiger_async import EigerDetector
+from nslsii.ophyd_async.providers import NSLS2PathProvider
 from ophyd_async.core import init_devices
-from ophyd_async.providers import NSLS2PathProvider
 
 pp = NSLS2PathProvider(RE.md)  # noqa: F821
 
-with init_devices(mock=True):
+with init_devices():
     eiger = EigerDetector(
         prefix="XF:09ID1-ES{Det:Eig1}",
         name="eiger",
