f7104ebf1e047f2a334e5297bd23d7408ac590ce
CDI Operator <xf09id1@xf09id1-ws3.nsls2.bnl.gov>
2026-03-23 13:42:58 -0400

    merlin work

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -1,16 +1,14 @@
 from pathlib import PosixPath
 
-from cditools.eiger_async import EigerDetector
-from cditools.merlin import CDIMerlinDetector
+#from cditools.eiger_async import EigerDetector
+from cditools.merlin_async import MerlinDetector
 from ophyd_async.core import UUIDFilenameProvider, YMDPathProvider, init_devices
-
-md = RE.md  # noqa: F821
-merlin = CDIMerlinDetector("XF:09ID1-ES{Det:Merlin1}", md, name="merlin")
 
 filename_provider = UUIDFilenameProvider()
 path_provider = YMDPathProvider(filename_provider, PosixPath("/tmp"))
 
-with init_devices(mock=True):
-    eiger = EigerDetector(
-        prefix="XF:09ID1-ES{Det:Eig1}", name="eiger", path_provider=path_provider
-    )
+with init_devices(mock=False):
+    merlin = MerlinDetector(prefix="XF:09ID1-ES{Det:Merlin1}", name="merlin", path_provider=path_provider)
+#     eiger = EigerDetector(
+#         prefix="XF:09ID1-ES{Det:Eig1}", name="eiger", path_provider=path_provider
+#     )
