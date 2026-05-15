259d765497f785fb9103bd3a7bb97d47a910a370
Dan Henriksen <dhenrikse@bnl.gov>
2026-05-15 14:55:05 -0400

    moved prefix from cditools to here

--- startup/22-attenuators.py
+++ startup/22-attenuators.py
@@ -3,5 +3,8 @@ print(f"Loading {__file__!r} ...")
 from cditools.attenuator import AttenuatorBank
 from ophyd_async.core import init_devices
 
+
+prefix = "XF:09ID1-ES{IOLOGIK1:E1212}"
+
 with init_devices():
-    bank = AttenuatorBank(energy)
+    bank = AttenuatorBank(prefix, energy)
