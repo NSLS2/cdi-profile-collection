b08d26d952b64cb797d683ae289c55d7327ae7c3
CDI Operator <xf09id1@xf09id1-ws3.nsls2.bnl.gov>
2026-06-03 14:07:43 -0400

    work from june 3rd, triggerRBV issue

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -1,4 +1,5 @@
 from cditools.eiger_async import EigerDetector
+from cditools.merlin_async import MerlinDetector
 from nslsii.ophyd_async.providers import NSLS2PathProvider
 from ophyd_async.core import init_devices
 
@@ -10,3 +11,6 @@ with init_devices():
     eiger = EigerDetector(
         prefix="XF:09ID1-ES{Det:Eig1}", name="eiger2-1", path_provider=pp
     )
+    # merlin = MerlinDetector(
+    #     prefix="XF:09ID1-ES{Det:Merlin1}", name="merlin", path_provider=pp
+    # )
