774754ab9154ff1326bfa4b1456c9796d580112c
CDI Operator <xf09id1@xf09id1-ws3.nsls2.bnl.gov>
2026-06-25 20:52:32 -0400

    ENH: get merlin to work

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -2,6 +2,7 @@ from cditools.eiger_async import EigerDetector
 from cditools.merlin_async import MerlinDetector
 from nslsii.ophyd_async.providers import NSLS2PathProvider
 from ophyd_async.core import init_devices
+from ophyd_async.epics import adcore
 
 print("LOADING 30")
 
@@ -12,5 +13,7 @@ with init_devices():
         prefix="XF:09ID1-ES{Det:Eig1}", name="eiger2-1", path_provider=pp
     )
     merlin = MerlinDetector(
-        prefix="XF:09ID1-ES{Det:Merlin1}", name="merlines-1", path_provider=pp
+        "XF:09ID1-ES{Det:Merlin1}", 
+        adcore.ADWriterFactory.hdf(pp, writer_suffix='HDF1:'),
+        name="merlines-1",
     )
