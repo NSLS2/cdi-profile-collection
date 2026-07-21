89276267ea80679107100611f9c2e5f70cb505d6
Thomas A Caswell <tcaswell@bnl.gov>
2026-07-21 17:23:52 -0400

    WIP: add cam1

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -1,8 +1,9 @@
 from cditools.eiger_async import EigerDetector
 from cditools.merlin_async import MerlinDetector
 from nslsii.ophyd_async.providers import NSLS2PathProvider
+
 from ophyd_async.core import init_devices
-from ophyd_async.epics import adcore
+from ophyd_async.epics import adcore, advimba
 
 print("LOADING 30")
 
@@ -17,3 +18,9 @@ with init_devices():
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="merlines-1",
     )
+
+    cam1 = advimba.VimbaDetector(
+        "XF:09IDA-BI{DM:1-Cam:1}",
+        adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
+        name="cam-1",
+        )
