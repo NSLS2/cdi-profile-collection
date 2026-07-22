2055bafae5961121ed511059a7d1369b11c937c3
Dan Henriksen <dhenrikse@bnl.gov>
2026-07-22 13:07:58 -0400

    overwrite vimba acquire logic to reset trigger mode

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -1,13 +1,58 @@
+from typing import Sequence
+
 from cditools.eiger_async import EigerDetector
 from cditools.merlin_async import MerlinDetector
 from nslsii.ophyd_async.providers import NSLS2PathProvider
 
-from ophyd_async.core import init_devices
+from ophyd_async.core import SignalR, init_devices
 from ophyd_async.epics import adcore, advimba
+from ophyd_async.epics.core import stop_busy_record
+
+import asyncio
 
 print("LOADING 30")
 
 pp = NSLS2PathProvider(RE.md)  # noqa: F821
+
+
+# TODO - move to cditools / nslsii
+class VimbaAcquireLogic(advimba.ADAcquireLogic):
+
+    async def ensure_ready(self):
+        self._cached_trigger_mode = await self.driver.trigger_mode.get_value()
+        await stop_busy_record(self.driver.acquire)
+
+    async def ensure_stopped(self):
+        if self._cached_trigger_mode is not None:
+            await self.driver.trigger_mode.set(self._cached_trigger_mode)
+        self._cached_trigger_mode = None
+        await stop_busy_record(self.driver.acquire)
+
+
+# TODO - rename and move to cditools / nslsii / ophyd-async
+class TmpAdvimba(advimba.VimbaDetector):
+
+    def __init__(
+            self,
+            prefix: str,
+            *writer_factories: advimba.ADWriterFactory,
+            driver_suffix="cam1:",
+            override_deadtime: float | None = None,
+            plugins: dict[str, advimba.NDPluginBaseIO] | None = None,
+            config_sigs: Sequence[SignalR] = (),
+            name: str = "",
+        ):
+        driver = advimba.VimbaDriverIO(prefix + driver_suffix)
+        super().__init__(
+            driver,
+            prefix,
+            *writer_factories,
+            acquire_logic=VimbaAcquireLogic(driver),
+            trigger_logic=advimba.VimbaTriggerLogic(driver, override_deadtime),
+            plugins=plugins,
+            config_sigs=config_sigs,
+            name=name,
+        )
 
 with init_devices():
     eiger = EigerDetector(
@@ -19,58 +64,58 @@ with init_devices():
         name="merlines-1",
     )
 
-    cam1 = advimba.VimbaDetector(
+    cam1 = TmpAdvimba(
         "XF:09IDA-BI{DM:1-Cam:1}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-1",
     )
-    cam2 = advimba.VimbaDetector(
+    cam2 = TmpAdvimba(
         "XF:09IDA-BI{WBStop-Cam:2}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-2",
     )
-    cam3 = advimba.VimbaDetector(
+    cam3 = TmpAdvimba(
         "XF:09IDA-BI{VPM-Cam:3}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-3",
     )
-    cam4 = advimba.VimbaDetector(
+    cam4 = TmpAdvimba(
         "XF:09IDA-BI{HPM-Cam:4}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-4",
     )
-    cam5 = advimba.VimbaDetector(
+    cam5 = TmpAdvimba(
         "XF:09IDA-BI{DM:2-Cam:5}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-5",
     )
-    cam6 = advimba.VimbaDetector(
+    cam6 = TmpAdvimba(
         "XF:09IDB-BI{DM:3-Cam:6}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-6",
     )
-    cam7 = advimba.VimbaDetector(
+    cam7 = TmpAdvimba(
         "XF:09IDC-BI{FS:KBv-Cam:7}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-7",
     )
 
-    cam8 = advimba.VimbaDetector(
+    cam8 = TmpAdvimba(
         "XF:09IDC-BI{FS:KBh-Cam:8}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-8",
     )
-    cam9 = advimba.VimbaDetector(
+    cam9 = TmpAdvimba(
         "XF:09IDC-BI{BCU-Cam:9}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-9",
     )
-    cam10 = advimba.VimbaDetector(
+    cam10 = TmpAdvimba(
         "XF:09IDC-BI{SMPL-Cam:10}",
         adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
         name="cam-10",
     )
-    # cam15 = advimba.VimbaDetector(
+    # cam15 = TmpAdvimba(
     #     "XF:09IDC-BI{Cam:15}",
     #     adcore.ADWriterFactory.hdf(pp, writer_suffix="HDF1:"),
     #     name="cam-15",
