37d1d8df327cfe0258ea90546e161bebefa14510
CDI Operator <xf09id1@xf09id1-ws1.nsls2.bnl.gov>
2026-08-04 08:04:02 -0400

    MNT: remove ophyd version of cams

--- startup/18-screens.py
+++ startup/18-screens.py
@@ -43,29 +43,7 @@ class MaskedCam(StandardProsilicaCam):
             self.roistat1: self.cam,
         }
 
-
-cam_A1 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{DM:1-Cam:1}", name="cam_A1"))
-cam_A2 = set_roiN_kinds(
-    StandardProsilicaCam("XF:09IDA-BI{WBStop-Cam:2}", name="cam_A2")
-)
-cam_A3 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{VPM-Cam:3}", name="cam_A3"))
-cam_A4 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{HPM-Cam:4}", name="cam_A4"))
-cam_A5 = set_roiN_kinds(StandardProsilicaCam("XF:09IDA-BI{DM:2-Cam:5}", name="cam_A5"))
-cam_B6 = set_roiN_kinds(StandardProsilicaCam("XF:09IDB-BI{DM:3-Cam:6}", name="cam_B6"))
-setup_centroids(cam_B6.stats1)
-cam_C7 = set_roiN_kinds(
-    StandardProsilicaCam("XF:09IDC-BI{FS:KBv-Cam:7}", name="cam_C7")
-)
-cam_C8 = set_roiN_kinds(
-    StandardProsilicaCam("XF:09IDC-BI{FS:KBh-Cam:8}", name="cam_C8")
-)
-cam_C9 = set_roiN_kinds(MaskedCam("XF:09IDC-BI{BCU-Cam:9}", name="cam_C9"))
-cam_C10 = set_roiN_kinds(MaskedCam("XF:09IDC-BI{SMPL-Cam:10}", name="cam_C10"))
 cam_C15 = set_roiN_kinds(MaskedCam("XF:09IDC-BI{Cam:15}", name="cam_C15"))
-
-for cam in [cam_C9, cam_C10, cam_C15]:
-    cam.stats5.total.kind = "hinted"
-    cam.stats5.kind = "normal"
 
 vpm_fs = StandardScreen("XF:09IDA-OP:1{FS:VPM-Ax:Y}Mtr", name="screen_vpm")
 hpm_fs = StandardScreen("XF:09IDA-OP:1{FS:HPM-Ax:Y}Mtr", name="screen_hpm")
