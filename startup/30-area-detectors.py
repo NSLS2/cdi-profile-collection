2fa902e7ab06fa3d3dcecbf4f43b3096d42d13ea
CDI Operator <xf09id1@xf09id1-ws3.nsls2.bnl.gov>
2026-08-04 10:11:05 -0400

    ENH: add a hacked version of stats plugin
    We need to get the controid information out, this seems beter than N single signal logics.

--- startup/30-area-detectors.py
+++ startup/30-area-detectors.py
@@ -3,11 +3,28 @@ from typing import Sequence
 
 from cditools.eiger_async import EigerDetector
 from cditools.merlin_async import MerlinDetector
+from event_model import DataKey
 from nslsii.ophyd_async.providers import NSLS2PathProvider
-from ophyd_async.core import SignalR, SignalRW, init_devices
+from ophyd_async.core import (
+    SignalR,
+    SignalRW,
+    StandardReadable,
+    StandardReadableFormat,
+    init_devices,
+)
+from ophyd_async.core._utils import merge_gathered_dicts
 from ophyd_async.epics import adcore, advimba
-from ophyd_async.epics.adcore import ADAcquireLogic, ADState, AreaDetector
-from ophyd_async.epics.core import EpicsOptions, PvSuffix, stop_busy_record
+from ophyd_async.epics.adcore import (
+    NDROIIO,
+    ADAcquireLogic,
+    ADState,
+    ADWriterFactory,
+    AreaDetector,
+    NDPluginBaseIO,
+    NDStatsIO,
+    PluginSignalDataLogic,
+)
+from ophyd_async.epics.core import EpicsDevice, EpicsOptions, PvSuffix, stop_busy_record
 
 print("LOADING 30")
 
@@ -20,7 +37,42 @@ class CustomVimbaDriverIO(advimba.VimbaDriverIO):
     acquire: A[SignalRW[bool], PvSuffix.rbv("Acquire"), EpicsOptions(wait=False)]
 
 
+class NDCentroid(StandardReadable, EpicsDevice):
+    X: A[
+        SignalR[float], PvSuffix("CentroidX_RBV"), StandardReadableFormat.HINTED_SIGNAL
+    ]
+    Y: A[
+        SignalR[float], PvSuffix("CentroidY_RBV"), StandardReadableFormat.HINTED_SIGNAL
+    ]
+    SigmaX: A[SignalR[float], PvSuffix("SigmaX_RBV"), StandardReadableFormat.CHILD]
+    SigmaY: A[SignalR[float], PvSuffix("SigmaY_RBV"), StandardReadableFormat.CHILD]
+
+    SkewX: A[SignalR[float], PvSuffix("SkewX_RBV"), StandardReadableFormat.CHILD]
+    SkewY: A[SignalR[float], PvSuffix("SkewY_RBV"), StandardReadableFormat.CHILD]
+
+    KurtosisX: A[
+        SignalR[float], PvSuffix("KurtosisX_RBV"), StandardReadableFormat.CHILD
+    ]
+    KurtosisY: A[
+        SignalR[float], PvSuffix("KurtosisY_RBV"), StandardReadableFormat.CHILD
+    ]
+
+    SigmaXY: A[SignalR[float], PvSuffix("SigmaXY_RBV"), StandardReadableFormat.CHILD]
+    Eccentricity: A[
+        SignalR[float], PvSuffix("Eccentricity_RBV"), StandardReadableFormat.CHILD
+    ]
+    Orientation: A[
+        SignalR[float], PvSuffix("Orientation_RBV"), StandardReadableFormat.CHILD
+    ]
+
+
+class EnrichedStats(NDStatsIO, StandardReadable):
+    centroid: A[NDCentroid, PvSuffix(""), StandardReadableFormat.CHILD]
+
+
 class VimbaAcquireLogic(ADAcquireLogic):
+    driver: CustomVimbaDriverIO
+
     async def ensure_ready(self):
         detector_state = await self.driver.detector_state.get_value()
         self._cached_acquire_state = detector_state != ADState.IDLE
@@ -46,16 +98,20 @@ class VimbaAcquireLogic(ADAcquireLogic):
 
 # TODO - move to ophyd-async
 class TmpAdvimba(AreaDetector[CustomVimbaDriverIO]):
+    stats1: EnrichedStats
+
     def __init__(
         self,
         prefix: str,
-        *writer_factories: advimba.ADWriterFactory,
+        *writer_factories: ADWriterFactory,
         driver_suffix="cam1:",
         override_deadtime: float | None = None,
-        plugins: dict[str, advimba.NDPluginBaseIO] | None = None,
+        plugins: dict[str, NDPluginBaseIO] | None = None,
         config_sigs: Sequence[SignalR] = (),
         name: str = "",
     ):
+        plugins = plugins or {}
+
         driver = CustomVimbaDriverIO(prefix + driver_suffix)
         super().__init__(
             driver,
@@ -63,10 +119,34 @@ class TmpAdvimba(AreaDetector[CustomVimbaDriverIO]):
             *writer_factories,
             acquire_logic=VimbaAcquireLogic(driver),
             trigger_logic=advimba.VimbaTriggerLogic(driver, override_deadtime),
-            plugins=plugins,
+            plugins={
+                "stats1": EnrichedStats(f"{prefix}Stats1:"),
+                "roi1": NDROIIO(f"{prefix}ROI1:"),
+                **plugins,
+            },
             config_sigs=config_sigs,
             name=name,
         )
+        self.add_detector_logics(PluginSignalDataLogic(driver, self.stats1.total))
+
+    async def read(self):
+        base = super().read()
+        local = merge_gathered_dicts(st.read() for st in [self.stats1])
+
+        return await merge_gathered_dicts([base, local])
+
+    async def describe(self) -> dict[str, DataKey]:
+        base = super().describe()
+        local = merge_gathered_dicts(st.describe() for st in [self.stats1])
+        return await merge_gathered_dicts([base, local])
+
+    @property
+    def hints(self):
+        base = super().hints
+        base["fields"] = list(set(base.get("fields", []) + self.stats1.hints["fields"]))
+        return base
+
+    # TODO need to add read_configuration and describe_configruation to read ROI plugins
 
 
 with init_devices():
